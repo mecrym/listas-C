8b1ca2dad79114a40c95a33adc0bfd2f13ef4d65
Maria Moreira <maria.ecr.moreira1@gmail.com>
2024-05-09 21:12:19 -0300

    questão 5 lista3 FIBONACCI

---
+++ lista3/q5.c
@@ -0,0 +1,24 @@
+/*A série de Fibonacci é formada pela sequência: 1, 1, 2, 3, 5, 8, 13, 21, 34, ... .
+Escreva um algoritmo que peça um número N maior que 2. Gere e imprima a série de 1 até este enésimo termo.*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int num, primeiro = 0 , segundo = 1, proximo=1;
+    printf("Digite um número: ");
+    scanf("%d", &num);
+    if (num>2)
+    {
+        for (int i = 0; i < num; i++)
+        {
+            printf("%d ", proximo);
+            proximo = primeiro + segundo;
+            primeiro = segundo;
+            segundo = proximo;
+        }
+    }else
+    {
+        printf("Número inválido\n");
+    }
+    return 0;
+}
