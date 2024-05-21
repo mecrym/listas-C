3627606d113b146329149763b54e47a2eaaaf12d
Maria Moreira <maria.ecr.moreira1@gmail.com>
2024-05-21 18:38:13 -0300

    teste vetor

---
+++ lista5/videoAula.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    float notas[5];
+    float media;
+
+    for (int i=0; i<5 ; i++)
+    {
+        printf("Digite a nota do aluno %d: ", i+1);
+        scanf("%f", &notas[i]);
+    }
+    media = (notas[0] + notas[1] + notas[2] + notas[3] + notas[4]) / 5;
+    printf("A media do aluno e: %2.f", media);
+    return 0;
+}
