0685e3d425c4f8b04fbd9547008584f79247d4cc
Maria Moreira <maria.ecr.moreira1@gmail.com>
2024-05-21 15:29:37 -0300

    lista5

--- lista5/q5_l5.c
+++ lista5/q5_l5.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
+void procedimentos(int x, int y);
 
-/*- ler dois valores inteiros positivos, limites para definirem um intervalo; ( esses valores deverão ser globais
-- ler uma quantidade de valores inteiros a serem testados – FLAG -1; um por vez;
-- contar e mostrar dentre esses valores lidos os que forem múltiplos de 2 e de 3, ao mesmo tempo, e pertençam ao intervalo.*/
-int main(){
+int main() {
     int x, y;
     printf("Digite dois valores inteiros positivos: ");
     scanf("%d %d", &x, &y);
@@ -14,30 +11,19 @@ int main(){
     return 0;
 }
 
-void procedimentos(x, y){
-    int c;
-    printf("Digite um valor inteiro: ");
-    if(x<y){
-        for(int a=0; a!=-1; a++){
-            scanf("%d", &c);
-            if(a==-1){
-                printf("Fim do programa. Valor digitado foi -1.");
-                break;
-            }
-            else if(a%2==0 && a%3==0 && a>=x && a<=y){
-                printf("O valor %d é múltiplo de 2 e 3 e pertence ao intervalo.\n", a);
-            }
-        }
-    }else if(y>x){
-        for(int a=0; a!=-1; a++){
-            scanf("%d", &c);
-            if(a==-1){
-                printf("Fim do programa. Valor digitado foi -1.");
-                break;
-            }
-            else if(a%2==0 && a%3==0 && a>=y && a<=x){
-                printf("O valor %d é múltiplo de 2 e 3 e pertence ao intervalo.\n", a);
-            }
+void procedimentos(int x, int y) {
+    int a;
+
+    for(int i = 0;a != -1; i++) {
+        printf("Digite um valor inteiro (-1 para terminar): ");
+        scanf("%d", &a);
+
+        if (a != -1 && a >= x && a <= y && a % 2 == 0 && a % 3 == 0) {
+            printf("O valor %d e multiplo de 2 e 3 e pertence ao intervalo.\n", a);
+        }else if(a==-1){
+            printf("Fim do programa.\n");
+        }else{
+            printf("O valor %d NAO e multiplo de 2 e 3 ou nao pertence ao intervalo.\n", a);
         }
     }
 }
